3156b371b4bc9e3361d83e50e14c06b56659584d
sai deepthi <saideepthi@sais-MacBook-Air.local>
2026-07-19 16:55:56 +0530

    Update backend API URL

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -18,7 +18,7 @@ if (registerForm) {
         }
 
         try {
-            const response = await fetch("http://localhost:5001/api/auth/register", {
+            const response = await fetch("https://pollsphere-qpj3.onrender.com/api/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
